feb1d42dc01567e95c458a3059ccc6e60ca2e99b
Felipe Sales <sales.felipeb@gmail.com>
2016-10-28 12:00:28 -0300

    Removendo os arquivos não usados, implantando versão compatível com celulares

--- less/compile.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-
-lessc --clean-css about.less > felipe.min.css
-lessc --clean-css experience.less >> felipe.min.css
-lessc --clean-css skills.less >> felipe.min.css
-lessc --clean-css main.less >> felipe.min.css
-lessc --clean-css sidebar.less >> felipe.min.css
-lessc --clean-css contact.less >> felipe.min.css
